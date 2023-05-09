2c524221f81e633942cbb142770c1b28f936b92b
Dwayne Robinson <rdwayne876@gmail.com>
2023-05-08 20:05:27 -0500

    main abandoned

--- src/app/rubiks-cube/rubiks-cube.component.ts
+++ src/app/rubiks-cube/rubiks-cube.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
+import * as TWEEN from '@tweenjs/tween.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
@@ -15,10 +16,12 @@ export class RubiksCubeComponent implements OnInit {
   private camera!: THREE.PerspectiveCamera;
   private renderer!: THREE.WebGLRenderer;
   private cubeSize = 3;
-  private cubes: THREE.Mesh[][][] = [];
+  private cubes: THREE.Group[][][] = [];
   private controls!: OrbitControls;
   private raycaster = new THREE.Raycaster();
   private mouse = new THREE.Vector2();
+  private cubeColors: (number | undefined)[][][][] = [];
+
 
 
   @ViewChild('rendererContainer', { static: true })
@@ -38,6 +41,9 @@ export class RubiksCubeComponent implements OnInit {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
     this.camera.position.z = 10;
+    const light = new THREE.PointLight(0xffffff, 1, 100);
+    light.position.set(10, 10, 10);
+    this.scene.add(light);
   }
 
   private initControls() {
@@ -50,29 +56,86 @@ export class RubiksCubeComponent implements OnInit {
   }
 
   private createRubiksCube() {
-    const geometry = new THREE.BoxGeometry(0.95, 0.95, 0.95);
+    const cubieSize = 1;
+    const spacing = 0.1;
+    const offset = (cubieSize + spacing) * this.cubeSize / 2 - (cubieSize / 2);
+  
     const materials = [
-      new THREE.MeshBasicMaterial({ color: 0xff0000 }), // right
-      new THREE.MeshBasicMaterial({ color: 0x00ff00 }), // left
-      new THREE.MeshBasicMaterial({ color: 0x0000ff }), // top
-      new THREE.MeshBasicMaterial({ color: 0xffff00 }), // bottom
-      new THREE.MeshBasicMaterial({ color: 0xffa500 }), // front
-      new THREE.MeshBasicMaterial({ color: 0xffffff })  // back
+      new THREE.MeshBasicMaterial({ color: 0xff0000 }), // right face - red
+      new THREE.MeshBasicMaterial({ color: 0x00ff00 }), // left face - green
+      new THREE.MeshBasicMaterial({ color: 0xffffff }), // top face - white
+      new THREE.MeshBasicMaterial({ color: 0xffff00 }), // bottom face - yellow
+      new THREE.MeshBasicMaterial({ color: 0xffa500 }), // front face - orange
+      new THREE.MeshBasicMaterial({ color: 0x0000ff }), // back face - blue
     ];
-
+  
     for (let x = 0; x < this.cubeSize; x++) {
       this.cubes[x] = [];
       for (let y = 0; y < this.cubeSize; y++) {
         this.cubes[x][y] = [];
         for (let z = 0; z < this.cubeSize; z++) {
-          const cube = new THREE.Mesh(geometry, materials);
-          cube.position.set(x - 1, y - 1, z - 1);
-          this.scene.add(cube);
-          this.cubes[x][y][z] = cube;
+          const cubie = new THREE.Group();
+  
+          for (let faceIndex = 0; faceIndex < 6; faceIndex++) {
+            if (
+              (faceIndex === 0 && x === this.cubeSize - 1) ||
+              (faceIndex === 1 && x === 0) ||
+              (faceIndex === 2 && y === this.cubeSize - 1) ||
+              (faceIndex === 3 && y === 0) ||
+              (faceIndex === 4 && z === this.cubeSize - 1) ||
+              (faceIndex === 5 && z === 0)
+            ) {
+              const faceGeometry = new THREE.PlaneGeometry(cubieSize, cubieSize);
+              const faceMaterial = new THREE.MeshBasicMaterial({ color: materials[faceIndex].color, side: THREE.DoubleSide });
+              const faceMesh = new THREE.Mesh(faceGeometry, faceMaterial);
+  
+              const rotationAxis = new THREE.Vector3(faceIndex % 2 === 0 ? 1 : 0, faceIndex < 2 ? 0 : 1, faceIndex < 4 ? 0 : 1);
+              const rotationAngle = faceIndex % 2 === 0 ? Math.PI / 2 : -Math.PI / 2;
+  
+              faceMesh.rotateOnWorldAxis(rotationAxis, rotationAngle);
+              faceMesh.translateOnAxis(rotationAxis, cubieSize / 2);
+              cubie.add(faceMesh);
+            }
+          }
+  
+          cubie.position.set(
+            x * (cubieSize + spacing) - offset,
+            y * (cubieSize + spacing) - offset,
+            z * (cubieSize + spacing) - offset
+          );
+  
+          this.cubes[x][y][z] = cubie;
+          this.scene.add(cubie);
+  
+          if (!this.cubeColors[x]) {
+            this.cubeColors[x] = [];
+          }
+          if (!this.cubeColors[x][y]) {
+            this.cubeColors[x][y] = [];
+          }
+          if (!this.cubeColors[x][y][z]) {
+            this.cubeColors[x][y][z] = [];
+          }
+          for (let faceIndex = 0; faceIndex < 6; faceIndex++) {
+            if (
+              (faceIndex === 0 && x === this.cubeSize - 1) ||
+              (faceIndex === 1 && x === 0) ||
+              (faceIndex === 2 && y === this.cubeSize - 1) ||
+              (faceIndex === 3 && y === 0) ||
+              (faceIndex === 4 && z === this.cubeSize - 1) ||
+              (faceIndex === 5 && z === 0)
+            ) {
+              this.cubeColors[x][y][z][faceIndex] = materials[faceIndex].color.getHex();
+            } else {
+              this.cubeColors[x][y][z][faceIndex] = undefined;
+            }
+          }
         }
       }
     }
   }
+  
+  
 
   private animate() {
     requestAnimationFrame(() => this.animate());
@@ -120,35 +183,90 @@ export class RubiksCubeComponent implements OnInit {
 
       const direction = clickedFaceNormal[axis] > 0 ? 1 : -1;
 
-      this.rotateLayer(clickedCubie, axis, direction * Math.PI / 2);
+      this.rotateLayer(clickedCubie, axis, direction * Math.PI / 2, 500);
     } else {
       this.controls.enabled = true;
     }
   }
 
-  private rotateLayer(clickedCubie: THREE.Object3D, axis: 'x' | 'y' | 'z', angle: number) {
+  private rotateLayer(clickedCubie: THREE.Object3D, axis: 'x' | 'y' | 'z', angle: number, duration: number) {
     const layerPosition = Math.round(clickedCubie.position[axis]);
-  
     const rotationAxis = axis === 'x'
       ? new THREE.Vector3(1, 0, 0)
       : axis === 'y'
-      ? new THREE.Vector3(0, 1, 0)
-      : new THREE.Vector3(0, 0, 1);
-  
-    const rotationMatrix = new THREE.Matrix4().makeRotationAxis(rotationAxis, angle);
+        ? new THREE.Vector3(0, 1, 0)
+        : new THREE.Vector3(0, 0, 1);
 
-    // Loop through all cubies
-    this.cubes.flat(2).forEach((cubie) => {
-      if (Math.round(cubie.position[axis]) === layerPosition) {
-        // Rotate the cubie
-        cubie.applyMatrix4(rotationMatrix);
-
-        // Round the position values to avoid floating-point issues
-        cubie.position.round();
+    this.animateRotation(layerPosition, axis, rotationAxis, angle, duration).onComplete(() => {
+      // ...
+      // Update cubeColors array with new positions
+      for (let x = 0; x < this.cubeSize; x++) {
+        for (let y = 0; y < this.cubeSize; y++) {
+          for (let z = 0; z < this.cubeSize; z++) {
+            this.cubeColors[x][y][z] = this.cubes[x][y][z].material.color.getHex();
+          }
+        }
       }
-    });
+      // Reassign the colors based on the updated cubeColors array
+      for (let x = 0; x < this.cubeSize; x++) {
+        for (let y = 0; y < this.cubeSize; y++) {
+          for (let z = 0; z < this.cubeSize; z++) {
+            this.cubes[x][y][z].material.color.set(this.cubeColors[x][y][z]);
+          }
+        }
+      }
+      // ...
+    });;
     
   }
 
-  
+  private animateRotation(layerPosition: number, axis: 'x' | 'y' | 'z', rotationAxis: THREE.Vector3, targetAngle: number, duration: number) {
+    const startTime = performance.now();
+    const layerCubies = this.cubes.flat(2).filter(cubie => Math.round(cubie.position[axis]) === layerPosition);
+
+    // Create a temporary parent object for the layer
+    const layerParent = new THREE.Object3D();
+
+    // Add the cubies in the layer to the parent object
+    layerCubies.forEach(cubie => {
+      this.scene.remove(cubie);
+      layerParent.add(cubie);
+    });
+
+    // Add the temporary parent object to the scene
+    this.scene.add(layerParent);
+
+    const animate = (currentTime: number) => {
+      const elapsedTime = currentTime - startTime;
+      const progress = Math.min(elapsedTime / duration, 1);
+      const currentAngle = targetAngle * progress;
+
+      // Reset the rotation of the parent object
+      layerParent.setRotationFromEuler(new THREE.Euler(0, 0, 0));
+
+      // Apply the new rotation to the parent object
+      layerParent.rotateOnAxis(rotationAxis, currentAngle);
+
+      this.renderer.render(this.scene, this.camera);
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      } else {
+        // Remove the cubies from the parent object and add them back to the scene
+        layerCubies.forEach(cubie => {
+          layerParent.remove(cubie);
+          this.scene.add(cubie);
+        });
+
+        // Remove the temporary parent object from the scene
+        this.scene.remove(layerParent);
+      }
+    };
+
+    requestAnimationFrame(animate);
+  }
+
+
+
+
 }
